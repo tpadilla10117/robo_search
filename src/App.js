9be36a49cf92d4623208f21aa52029c180ba2d13
Trinidad Padilla <tpadilla101@live.com>
2021-04-30 13:57:07 -0700

    cleaned up project

--- src/App.js
+++ src/App.js
@@ -11,28 +11,17 @@ class App extends Component {
     super();
     /* Here we initialize a value for the default */
     this.state = {
-      people: [
-        /* { name: "Drake", id: 'asc1'},
-        { name: "Francis", id: 'asc2'}, 
-        {name: "Dutch", id: 'asc3'} */
-      ],
+      people: [],
       searchField: ''
     };
-
-    /* Context of 'this' here is explicitly stated: */
-    /* this.handleChange = this.handleChange.bind(this); */
   }
 
-  /* 1) fetch() lets us make an API request to the URL */
-  /* 2) the fetch returns a promise, gives a response of the 'body' */
-  /* 3) we indicate that we want json to be returned from the response, which gets returned as a new response */
-  /* 4) We set our state to the 'users' we fetched */
+  /* We set our state to the 'users' we fetched */
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json())
     .then(users =>  this.setState({ people: users}));
   }
 
-  /* arrow function lets us bind this (lexical scoping) */
   handleChange = (element) => {
     this.setState({ searchField: element.target.value})
   }
@@ -46,36 +35,9 @@ class App extends Component {
       <h1>Robots Rolodex</h1>
       <SearchBox placeholder="search robots" handleChange={this.handleChange} />
       <CardList people={filteredPeople}/>
-     
-      <header className="App-header">
-       
-        <button onClick={() => this.setState({ string: 'Hello My Boi!'})}>Change Text</button>
-       
-      </header>
     </div>
     );
   }
 }
-
-/* function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-} */
 
 export default App;
--- src/components/search-box/searchbox.js
+++ src/components/search-box/searchbox.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import './searchbox.css';
 
-//functional components don't have access to internal state (don't have constructor) or life-cycle methods
-// sometimes all we want is to render html
-//just a component that gets some propsand returns html
 export const SearchBox = ({ placeholder, handleChange}) => (
     <input className="search" type='search' placeholder={placeholder} onChange={handleChange} />
 )
